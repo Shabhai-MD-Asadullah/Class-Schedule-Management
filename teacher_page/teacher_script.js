72a6f9e98d5bd8b24670f1fdfb721138da723d23
Shabhai Md Asadullah <ShabhaiMDAsadullah@gmail.com>
2025-10-01 23:26:16 +0530

    adding teacher page files

---
+++ teacher_page/teacher_script.js
@@ -0,0 +1,19 @@
+
+
+// Set today’s date
+document.getElementById("today-date").textContent = new Date().toDateString();
+function showTab(tabId, element) {
+  document
+    .querySelectorAll(".content")
+    .forEach((c) => c.classList.remove("active-content"));
+  document
+    .querySelectorAll(".tab")
+    .forEach((t) => t.classList.remove("active-tab"));
+  document.getElementById(tabId).classList.add("active-content");
+  element.classList.add("active-tab");
+}
+
+function endPeriod() {
+  alert("Period ended. Next lecturer has been notified.");
+  // Replace with AJAX or PHP logic later
+}
